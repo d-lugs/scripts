bbdf62b1db198be829e1f7f8c59d096205397987
d-lugs <61252144+d-lugs@users.noreply.github.com>
2025-07-29 12:33:48 -0400

    Create alert-discord.sh

---
+++ alert-discord.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Send a message to Discord via POST request
+# Usage: alert-discord "enter message here"
+#
+# Requires a variables file with the following line:
+# ALERT_WEBHOOK_URL="https://discord.com/api/webhooks/<your webhook token here>"
+
+. /root/discord-vars
+
+MESSAGE=$1
+
+DATA='{"content":"'$MESSAGE'"}'
+
+curl -s -X POST \
+    -H "Content-Type: application/json" \
+    -d "${DATA}" \
+    $ALERT_WEBHOOK_URL
