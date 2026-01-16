c194a0510f49a57d8ec3b248226fa853edae3189
d-lugs <dclustig28@gmail.com>
2026-01-15 20:12:30 -0500

    reworking backup script

--- backup.sh
+++ backup.sh
@@ -1,16 +1,30 @@
 #!/usr/bin/bash
 
-# Initial bash script copied from:
-# https://www.slingacademy.com/article/ubuntu-how-to-write-an-rsync-backup-script/
+# rsync backup script
 
-log="${HOME}/backup/rsync_backup_
-.log"dest="${HOME}/backup/destination"
-src="/source/directory"
+log="/var/log/rsync_backup/rsync_backup_$(date +"%Y%m%d").log"
+include_file="${HOME}/rsync-include.txt"
+password_file="/root/backup/rsync_password.txt"
+exclude_file="/root/backup/rsync_exclude.txt"
+dest="rsync://rsync@qnap.lan:/Backup/$HOSTNAME"
+src_list=(
+    "/etc"
+    "/home"
+    "/opt"
+    "/root"
+    "/usr/local"
+    "/var/lib"
+)
 
 # Start backup
-{ 
+{
     echo "Starting backup: $(date '+%Y-%m-%d %H:%M:%S')";
-    rsync -avz --delete --exclude-from='${HOME}/rsync-exclude.txt' "${src}" "${dest}";
+    for path in ${src_list[@]}; do
+        rsync -varp --delete \
+            --password-file "${password_file}" \
+            --exclude-from="${exclude_file}" \
+            "${path}" "${dest}";
+    done
     echo "Backup completed: $(date '+%Y-%m-%d %H:%M:%S')";
 } | tee -a "${log}"
 
