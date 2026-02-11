60deea1a43afaea700b88d5474da9ddc53f38779
d-lugs <dclustig28@gmail.com>
2026-02-10 23:51:27 -0500

    rework backup script and add alerting

--- rsync/backup.sh
+++ rsync/backup.sh
@@ -1,32 +1,85 @@
 #!/usr/bin/bash
 
 # rsync backup script
+# Usage: ./backup.sh [OPTIONS]
+# Options:
+#
+# -a, --alert   Send discord alert (optional) requires .env with the following variables:
+#                   USERID              Discord user id (for tagging)
+#                   ALERT_WEBHOOK_URL   Discord webhook URL
 
 # Get absolute path of script
 path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P)
 cd $path
 
-. .env
-export RSYNC_PASSWORD=$RSYNC_PASSWORD
-
 log_file="/var/log/rsync/backup_$(date +"%Y%m%d").log"
 include_file="./include.txt"
 exclude_file="./exclude.txt"
 
-# Start backup
-echo "Starting backup: $(date '+%Y-%m-%d %H:%M:%S')"
-rsync -varp --delete --relative \
-    --files-from="${include_file}" \
-    --exclude-from="${exclude_file}" \
-    --log-file="${log_file}" \
-    "/" "rsync://rsync@$RSYNC_HOST:/Backup/$HOSTNAME"
-status=$?
-echo "Backup completed: $(date '+%Y-%m-%d %H:%M:%S')"
+# parse arguments
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -a|--alert)
+            notify="true"
+            shift # past argument
+            shift # past value
+            ;;
+        -*|--*)
+            echo "Unknown option $1"
+            exit 1
+            ;;
+        *)
+            POSITIONAL_ARGS+=("$1") # save positional arg
+            shift # past argument
+            ;;
+    esac
+done
 
-# Error handling
-if [ $status -ne 0 ]; then
-    echo "Rsync failed with exit code $status" | tee -a "${log_file}"
-    exit $status
-else
-    echo "Rsync completed successfully." | tee -a "${log_file}"
-fi
+send_alert() {
+    if [ "$notify" == "true" ]; then
+        local DISCORD_USERID ALERT_WEBHOOK_URL
+        . .env
+
+        msg="$@"
+        echo $msg
+
+        curl -s -X POST \
+            -F "username=$HOSTNAME" \
+            -F 'content="<@'"$DISCORD_USERID"'>
+'"$msg"'"' \
+            $ALERT_WEBHOOK_URL
+
+        if [ $? -ne 0 ]; then
+            echo -e "Failed to send Discord alert"
+        fi
+    else
+        echo -e $1
+    fi
+}
+
+check_failure(){
+    status=$?
+    if [ $status -ne 0 ]; then
+        send_alert "ERROR: Rsync backup failed (exit code: $status)"
+        exit 1
+    fi
+}
+
+{
+    . .env
+    export RSYNC_PASSWORD=$RSYNC_PASSWORD
+
+    start_time=$(date +"%s")
+    echo "$(date '+%Y/%m/%d %H:%M:%S') Starting backup"
+    rsync -arp --delete --relative \
+        --files-from="${include_file}" \
+        --exclude-from="${exclude_file}" \
+        --log-file="${log_file}" \
+        --list-only \
+        --quiet \
+        "/" "rsync://rsync@$RSYNC_HOST:/Backup/$HOSTNAME"
+    check_failure
+    send_alert "$(date '+%Y/%m/%d %H:%M:%S') Backup completed (Time elapsed: $(($(date +"%s")-$start_time)) seconds)"
+} | tee -a "$log_file" 2>&1
+
+exit 0
