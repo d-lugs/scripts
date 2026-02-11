ec307489c1887ba35ee46ac510f161d17e599b92
d-lugs <dclustig28@gmail.com>
2026-02-10 23:51:50 -0500

    reorg repo (again)

---
+++ alert-discord.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Send a message to Discord via POST request
+# Usage: alert-discord "enter message here"
+#
+# Requires a variables file with the following line:
+# ALERT_WEBHOOK_URL="https://discord.com/api/webhooks/<your webhook token here>"
+
+. ./.env
+
+MESSAGE=$@
+
+DATA='{"content":"'$MESSAGE'"}'
+
+curl -s -X POST \
+    -H "Content-Type: application/json" \
+    -d "${DATA}" \
+    $ALERT_WEBHOOK_URL
